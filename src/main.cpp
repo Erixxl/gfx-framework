b0b7e90cb40afac80b330542d9d93a33f4e0f05f
Andrei <theerixxl@gmail.com>
2024-12-03 13:57:22 +0200

    Lab 7

--- src/main.cpp
+++ src/main.cpp
@@ -47,10 +47,11 @@ int main(int argc, char **argv)
 	(void)Engine::Init(wp);
 
 	// Create a new 3D world and start running it
-	World *world = new m1::Lab6();
+	// World *world = new m1::Lab6();
+	// World *world = new m1::Lab7();
 	// World *world = new m1::Lab3_Vis2D();
 
-	// World* world = new hw1::Homework1();
+	World* world = new hw1::Homework1();
 	// World* world = new srpk::Sierpinski();
 
 	world->Init();
